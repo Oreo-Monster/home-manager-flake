9e00ba7a0bd73b5fc3de92a4bddddfc327bf3438
OreoMonster <ewrong16@gmail.com>
2024-09-28 22:59:36 -0500

    2024-09-28 22:36 : id 51 -> /nix/store/d1a1s5hlm14f6z0i2724xmngci46f7kq-home-manager-generation

--- modules/rebuild-script.nix
+++ modules/rebuild-script.nix
@@ -3,7 +3,7 @@
 
   config = let
     #Script taken from noboilerplate https://github.com/0atman/noboilerplate/blob/main/scripts/38-nixos.md
-    rebuildScript = pkgs.writeShellScriptBin "hm-rebuild" ''
+    homeManagerBuild = pkgs.writeShellScriptBin "hm-build" ''
       set -e #exit on an error
       pushd ~/.home-manager
 
@@ -51,9 +51,57 @@
 
       popd
     '';
+    nixBuild = pkgs.writeShellScriptBin "nix-build" ''
+      set -e #exit on an error
+      pushd ~/.nixos
+
+      # Early return if no changes were detected (thanks @singiamtel!)
+      echo -e "===================================\n  Detecting Changes...\n==================================="
+      git diff --minimal --ignore-blank-lines --color -U0 '*.nix' | cat
+      if git diff --quiet '*.nix'; then
+          read -p "No changes detected, build anyways? (y|N) " -n 1 -r
+          echo
+          if [[ ! $REPLY =~ ^[Yy]$ ]]
+          then
+              echo "No changes detected, exiting."
+              popd
+              exit 0
+          fi
+      fi
+
+      #If not already flagged
+      if [[ ! $REPLY =~ ^[Yy]$ ]]
+      then
+          read -p "Continue? (Y|n)" -n 1 -r
+          echo
+          if [[ ! $REPLY =~ ^[Yy]$ ]]
+              then
+                  echo "exiting..."
+                  popd
+                  exit 0
+          fi
+      fi
+
+      git add .
+
+      # Autoformat your nix files
+      echo -e "===================================\nFormating...\n==================================="
+      ${pkgs.alejandra}/bin/alejandra . &>/dev/null \
+        || ( ${pkgs.alejandra}/bin/alejandra . ; echo "formatting failed!" && exit 1)
+
+      echo -e "===================================\nRebuilding home-manager...\n==================================="
+      sudo nixos-rebuild switch --flake .#default &>.nixos-switch.log || (cat .nixos-switch.log | grep --color error && exit 1)
+      current=$(nixos-rebuild list-generations | grep current)
+
+      echo -e "===================================\nCommiting changes...\n==================================="
+      git commit -am "$current"
+      ${pkgs.libnotify}/bin/notify-send -e "Home Manager Succesfully Rebuild"
+      popd
+    '';
   in {
     home.packages = [
-      rebuildScript
+      homeManagerBuild
+      nixBuild
     ];
   };
 }
